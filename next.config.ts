0862aa4801b6a52c0fa551d366e71eae2ff79a07
Kishan M Mohan <madhanmohankishan@gmail.com>
2026-02-25 22:47:38 +0530

    Remove GitHub Actions `Setup Pages` workflow step and add a `basePath` comment to `next.config.ts`.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+// If you use a custom domain, you should not have a basePath.
+
 const nextConfig: NextConfig = {
   output: "export",
   // Disable image optimization since we are using static export without a custom loader
